c1c8e230de16ee81193bb0f2b6f30305993723c3
Sam McC-K <sam@mccardal.com>
2014-09-14 21:10:52 +0100

    tried to make highlighting work, almost there...

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
 $( document ).ready(function() {
-	//newline counts as a char
-	$("#editor").keyup(function() {
-		var text = $("#editor").text();
-		var lastTagEnd = 0;
-		for (i = 0; i < text.length; i++) {
-			if (text.charAt(i) == "<") {
-				for (j = i; j < text.length; j++) {
-					if (text.charAt(j) == ">") {
-
-						var tagText = splitter(i, j, lastTagEnd, text);
-						$("#editor").text("");
-						$("#editor").append("<div>" + tagText[0] + "</div>");
-						$("#editor").append("<span>" + tagText[1] + "</span>");
-						$("#editor").append("<div>" + tagText[2] + "</div>");
-
-						lastTagEnd = j;
-						break;
-					}
-				}
-				splitter(i, j, lastTagEnd, text);
-				lastTagEnd = j;
-			}
-		}
-	});
-
-	function splitter(startTag, endTag, startPos, code) {
-		var splitCode = [];
-		splitCode[0] = code.substring(startPos, startTag);
-		splitCode[1] = code.substring(startTag, endTag + 1);
-		splitCode[2] = code.substring(endTag + 1, code.length);
-		return splitCode;
-	}
+    //newline counts as a char
+    $("#editor").keyup(function() {
+        var text = $("#editor").text();
+        console.log(highlight(text));
+    });
+    
+    function highlight(text) {
+        for (i = 0; i < text.length; i++) {
+            if (text.charAt(i) == "<") {
+                for (j = i; j < text.length; j++) {
+                    if (text.charAt(j) == ">") {
+                        var returner = splitter(i, j, text);
+                        text = returner[0] + "<span>" + returner[1] + "</span>" + returner[2];
+                    }
+                }
+            }
+        }
+        return text;
+    }
+       
+    function splitter(i, j, text) {
+       return [text.substring(0, i), text.substring(i, j + 1), text.substring(j + 1, text.length)]; 
+    }
 });
