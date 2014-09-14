ebf0c3c3531f02983f05cdba97a1adcd4beb059a
Sam McC-K <sam@mccardal.com>
2014-09-14 21:38:39 +0100

    hopefully script is better now maybe probably not

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ $( document ).ready(function() {
         var text = $("#editor").text();
         console.log(highlight(text));
     });
-    
+    var start = 0; 
     function highlight(text) {
         for (i = 0; i < text.length; i++) {
             if (text.charAt(i) == "<") {
                 for (j = i; j < text.length; j++) {
                     if (text.charAt(j) == ">") {
-                        var returner = splitter(i, j, text);
+                        var returner = splitter(start, i, j, text);
                         text = returner[0] + "<span>" + returner[1] + "</span>" + returner[2];
-                        return text;
+                        start = j;
                     }
                 }
             }
@@ -20,7 +20,7 @@ $( document ).ready(function() {
         return text;
     }
        
-    function splitter(i, j, text) {
-       return [text.substring(0, i), text.substring(i, j + 1), text.substring(j + 1, text.length)]; 
+    function splitter(start, i, j, text) {
+       return [text.substring(start, i), text.substring(i, j + 1), text.substring(j + 1, text.length)]; 
     }
 });
