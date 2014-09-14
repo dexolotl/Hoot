1cddc8313e1b38fbe3950fd9b91efa5b87042e6b
Sam McC-K <sam@mccardal.com>
2014-09-14 22:04:53 +0100

    script now finds all pairs of tags!

--- script.js
+++ script.js
@@ -10,9 +10,11 @@ $( document ).ready(function() {
             if (text.charAt(i) == "<") {
                 for (j = i; j < text.length; j++) {
                     if (text.charAt(j) == ">") {
-                        var returner = splitter(start, i, j, text);
-                        text = returner[0] + "<span>" + returner[1] + "</span>" + returner[2];
-                        start = j;
+                        //var returner = splitter(start, i, j, text);
+                        //text = returner[0] + "<span>" + returner[1] + "</span>" + returner[2];
+                        //start = j;
+                        console.log(i, j);
+                        break;
                     }
                 }
             }
