fcd7f7d354830b6e8ae2c5a9effd80ebd350c83e
Sam McC-K <sam@mccardal.com>
2014-09-14 22:46:16 +0100

    improved point-finding

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $( document ).ready(function() {
     });
     var start = 0; 
     function highlight(text) {
+        var highlighted = "";
         for (i = 0; i < text.length; i++) {
             if (text.charAt(i) == "<") {
                 for (j = i; j < text.length; j++) {
@@ -13,12 +14,13 @@ $( document ).ready(function() {
                         //var returner = splitter(start, i, j, text);
                         //text = returner[0] + "<span>" + returner[1] + "</span>" + returner[2];
                         //start = j;
-                        console.log(i + " " + j + " " + splitter(start,i,j,text));
+                        highlighted += (i + " " + j + " " + splitter(start,i,j,text) + "\n");
                         break;
                     }
                 }
             }
         }
+        return highlighted;
     }
        
     function splitter(start, i, j, text) {
